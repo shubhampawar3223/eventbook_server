a1e3ca89ce8ec69e0d71547f44cb1ee9d521dbf8
unknown <shubhganeshan@gmail.com>
2020-09-01 19:35:14 +0530

    initial

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    trim:true,
+    required: true
+  },
+  email: {
+    type: String,
+    trim:true,
+    required: true
+  },
+  password: {
+    type: String,
+    trim:true,
+    required: true
+  }
+});
+module.exports = User = mongoose.model("users", UserSchema);
