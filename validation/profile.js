1f61aab5ee5a71f41ac55ea6befab8681587835f
shubhampawar3223 <68299278+shubhampawar3223@users.noreply.github.com>
2020-09-05 23:59:26 +0530

    fgfg

--- validation/profile.js
+++ validation/profile.js
@@ -14,7 +14,9 @@ module.exports= function  validateProfileInput(data){
     data.gradYear = !isEmpty(data.gradYear) ? data.gradYear : "";
     data.gender = !isEmpty(data.gender) ? data.gender : "";
     data.mobileNo = !isEmpty(data.mobileNo) ? data.mobileNo : "";
-    data.interests = !isEmpty(data.interests) ? data.interests : "";
+    data.interest1 = !isEmpty(data.interest1) ? data.interest1 : "";
+    data.interest2 = !isEmpty(data.interest2) ? data.interest2 : "";
+    data.interest3 = !isEmpty(data.interest3) ? data.interest3 : "";
 
     if (Validator.isEmpty(data.name)) {
         errors.name = "Name field is required";
@@ -26,34 +28,19 @@ module.exports= function  validateProfileInput(data){
         errors.email = "Email is invalid";
       }
     
-      if (Validator.isEmpty(data.password)) {
+      if(Validator.isEmpty(data.password)) {
         errors.password = "Password field is required";
       }
     
-    if(Validator.isEmpty(data.firstName)){
-        errors.FirstName= "First Name is required"
-    }
-    if(Validator.isEmpty(data.lastName)){
-        errors.LastName= "Last Name is required"
-    }
-    if(Validator.isEmpty(data.dob)){
-        errors.DOB= "DOB is required"
-    }
-    if(Validator.isEmpty(data.collageName)){
-        errors.CollageName= "Collage Name is required"
-    }
-    if(Validator.isEmpty(data.gradYear)){
-        errors.PassingYear= "Passing year is required"
-    }
-    if(Validator.isEmpty(data.gender)){
-        errors.Gender= "Gender is required"
-    }
-    if(Validator.isEmpty(data.mobileNo)){
-        errors.MobNo= "Mobile No is required"
-    }
-     if(Validator.isEmpty(data.interests)){
-         errors.interests= "Interests are required"
+     if(Validator.isEmpty(data.interest1)){
+         errors.interest1= "Interests are required"
      }
+     if (Validator.isEmpty(data.interest2)) {
+        errors.interest2 = "Password field is required";
+      }
+      if (Validator.isEmpty(data.interest3)) {
+        errors.interest3 = "Password field is required";
+      }
   return{
       errors,
       isValid: isEmpty(errors)
